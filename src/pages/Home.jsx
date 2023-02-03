303a8a46833d3552b125d1f341ac2200d6a3d071
DeeeeeeeX <parabala58@gmail.com>
2023-02-04 01:08:58 +0300

    8 lesson adative, layout, basket

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,31 +5,32 @@ import PizzaBlock from "../components/PizzaBlock";
 import {useEffect, useState} from "react";
 
 const Home = () => {
-  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+    const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    fetch('https://63dc382dc45e08a04356e0d4.mockapi.io/items')
-      .then((res) => res.json())
-      .then((arr) => {
-        setItems(arr)
-        setIsLoading(false)
-      })
-  }, [])
-  return (
-    <>
-      <div className="content__top">
-        <Categories/>
-        <Sort/>
-      </div>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(8)].map((_, index) => <Skeleton key={index}/>)
-          : items.map(obj => (<PizzaBlock key={obj.id} {...obj} />))}
-      </div>
-    </>
-  )
+    useEffect(() => {
+        fetch('https://63dc382dc45e08a04356e0d4.mockapi.io/items')
+            .then((res) => res.json())
+            .then((arr) => {
+                setItems(arr)
+                setIsLoading(false)
+            })
+        window.scrollTo(0, 0)
+    }, [])
+    return (
+        <div className="container">
+            <div className="content__top">
+                <Categories/>
+                <Sort/>
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+                {isLoading
+                    ? [...new Array(8)].map((_, index) => <Skeleton key={index}/>)
+                    : items.map(obj => (<PizzaBlock key={obj.id} {...obj} />))}
+            </div>
+        </div>
+    )
 }
 
 export default Home
